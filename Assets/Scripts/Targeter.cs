aeafaa07b2acb1d048c9af565c8fe18bc293b1ab
LinkedLemon <Keeganpaxton2006@gmail.com>
2024-12-12 11:59:39 -0700

    Gravity gun and visual level

--- Assets/Scripts/Targeter.cs
+++ Assets/Scripts/Targeter.cs
@@ -6,20 +6,35 @@ public class Targeter : MonoBehaviour
     [SerializeField] private Transform targetOrigin;
     [SerializeField] private float targetDistance = 1000f;
     [SerializeField] private LayerMask hitLayer;
+    [SerializeField] private FixedJoint holderPosition;
     private AimTarget _currentTarget;
+    private bool touching;
     void Update()
     {
         Vector3 origin = transform.position;
         RaycastHit hit;
+        AimTarget _target;
+
         if (Physics.Raycast(origin, targetOrigin.transform.forward, out hit, targetDistance, hitLayer))
         {
             if (hit.collider.gameObject.TryGetComponent<AimTarget>(out AimTarget target))
             {
                 SwapTarget(target);
+                touching = true;
+                if (touching && Input.GetButtonDown("Fire1"))
+                {
+                    holderPosition.connectedBody = hit.rigidbody;
+                    Debug.Log("Grabbed");
+                }
+
+
+
                 return;
             }
         }
+
         _currentTarget?.StopTarget();
+        touching = false;
         _currentTarget = null;
 
     }
